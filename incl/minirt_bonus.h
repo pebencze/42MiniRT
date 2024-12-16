52e25e37612519ed13aca12eedcf08cda602a6be
pebencze <petra.bncz@gmail.com>
2024-12-16 16:47:07 +0100

    antialiasing started, but several function changes required to continue

--- incl/minirt_bonus.h
+++ incl/minirt_bonus.h
@@ -6,7 +6,7 @@
 /*   By: pbencze <pbencze@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 14:36:40 by vados-sa          #+#    #+#             */
-/*   Updated: 2024/12/16 12:07:26 by pbencze          ###   ########.fr       */
+/*   Updated: 2024/12/16 16:24:55 by pbencze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@
 # define CHECKP 1
 # define CHECKS 0
 # define SPECULAR 1
+# define ANTIALIAS 1
 
 # include "minirt.h"
 
 t_color		checkerboard(t_intersec *object);
 t_color		specular_light(t_l *light, t_intersec *hit_rec, t_data *data);
 t_coord		vec_reflect(t_coord incident, t_coord normal);
+uint32_t	calculate_color(uint32_t x, uint32_t y, t_data *data, t_ray ray);
 
 #endif
--- src/bonus/render_bonus.c
+++ src/bonus/render_bonus.c
@@ -6,11 +6,11 @@
 /*   By: pbencze <pbencze@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 14:47:00 by vados-sa          #+#    #+#             */
-/*   Updated: 2024/12/13 10:14:48 by pbencze          ###   ########.fr       */
+/*   Updated: 2024/12/16 16:45:50 by pbencze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minirt.h"
+#include "minirt_bonus.h"
 
 uint32_t	create_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
@@ -32,7 +32,7 @@ t_color	gradient(t_ray r, t_coord camera_up, t_data *data)
 	return (color);
 }
 
-void	put_color(uint32_t x, uint32_t y, t_data *data, t_ray ray)
+uint32_t	calculate_color(uint32_t x, uint32_t y, t_data *data, t_ray ray)
 {
 	uint32_t	col_int;
 	t_color		col_rgb;
@@ -48,7 +48,29 @@ void	put_color(uint32_t x, uint32_t y, t_data *data, t_ray ray)
 	else
 		col_rgb = gradient(ray, data->scene->c.up, data);
 	col_int = create_color(col_rgb.r, col_rgb.g, col_rgb.b, col_rgb.a);
-	mlx_put_pixel(data->image, x, y, col_int);
+	return (col_int);
+}
+
+void	antialias(uint32_t x, uint32_t y, t_data *data)
+{
+	uint32_t			color;
+	uint32_t			i;
+	uint32_t			j;
+	t_ray				ray;
+
+	i = -1;
+	color = 0;
+	while (++i < 8)
+	{
+		j = -1;
+		while (++j < 8)
+		{
+			ray = create_ray(x + (j / 8.0), y + (i / 8.0), data->scene->c.center, data);
+			color += calculate_color(x + (j / 8.0), y + (i / 8.0), data, ray);
+		}
+	}
+	color = color / 64;
+	mlx_put_pixel(data->image, x, y, color);
 }
 
 void	render(t_data *data)
@@ -56,8 +78,6 @@ void	render(t_data *data)
 	uint32_t	x;
 	uint32_t	y;
 	t_ray		ray;
-	uint32_t	color;
-	t_color		rgba;
 
 	y = -1;
 	while (++y < data->image_height)
@@ -65,8 +85,16 @@ void	render(t_data *data)
 		x = -1;
 		while (++x < data->image_width)
 		{
-			ray = create_ray(x, y, data->scene->c.center, data);
-			put_color(x, y, data, ray);
+			if (ANTIALIAS == 1)
+			{
+				antialias(x, y, data);
+			}
+			else
+			{
+				ray = create_ray(x, y, data->scene->c.center, data);
+				mlx_put_pixel(data->image, x, y, \
+				calculate_color(x, y, data, ray));
+			}
 		}
 	}
 }
